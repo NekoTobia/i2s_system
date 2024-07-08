8a1ed3d5a0bfffeefb3a4f0ca965ded61d13dc49
kevinlai920219 <106069613+kevinlai920219@users.noreply.github.com>
2024-07-08 11:01:26 +0800

    Add files via upload

---
+++ android_front_end/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Camera_x"
+include(":app")
+ 
